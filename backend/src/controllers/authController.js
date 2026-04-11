1da44ada3661294b973220fad7ba7bf71a25e785
jagriti-aswal <jagritiaswal12@gmail.com>
2026-04-12 02:00:00 +0530

    debugging2

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,42 +1,3 @@
-// import bcrypt from "bcryptjs";
-// import jwt from "jsonwebtoken";
-// import User from "../models/User.js";
-
-// export const loginUser = async (req, res) => {
-//   try {
-//     const { email, password, type } = req.body;
-
-//     const user = await User.findOne({ email, role: type });
-//     if (!user) {
-//       return res.status(401).json({ message: "Invalid email or password" });
-//     }
-
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) {
-//       return res.status(401).json({ message: "Invalid email or password" });
-//     }
-
-//     const token = jwt.sign(
-//       { id: user._id, role: user.role },
-//       process.env.JWT_SECRET,
-//       { expiresIn: "1d" }
-//     );
-
-//     res.json({
-//       success: true,
-//       token,
-//       user: {
-//         email: user.email,
-//         role: user.role,
-//       },
-//     });
-
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: "Server error" });
-//   }
-// };
-
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
@@ -55,10 +16,10 @@ export const login = async (req, res) => {
   }
 
   const token = jwt.sign(
-    { id: user._id, role: user.role },
-    process.env.JWT_SECRET,
-    { expiresIn: "1d" }
-  );
+  { id: user._id, role: user.role },
+  process.env.JWT_SECRET || "jwtkey",
+  { expiresIn: "1d" }
+);
 
   res.json({
     token,
