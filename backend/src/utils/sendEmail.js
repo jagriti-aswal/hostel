7859c3a5256389ec2fde2d4cec9c7b879886c757
Nishita-16 <nishuverma105514@gmail.com>
2026-04-13 10:38:18 +0530

    commit1

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,12 +1,34 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// export const sendEmail = async (to, name) => {
+//   try {
+//     await transporter.sendMail({
+//       from: `"Smart Hostel" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject: "⚠️ Attendance Reminder",
+//       html: `
+//         <h2>Hello ${name}</h2>
+//         <p>You have not marked your attendance today.</p>
+//         <p>Please mark it before deadline.</p>
+//         <br/>
+//         <p>— Smart Hostel System</p>
+//       `,
+//     });
+
+//     console.log(`📧 Email sent to ${to}`);
+//   } catch (err) {
+//     console.error("Email error:", err);
+//   }
+// };
+
 
 export const sendEmail = async (to, name) => {
   try {
@@ -18,8 +40,6 @@ export const sendEmail = async (to, name) => {
         <h2>Hello ${name}</h2>
         <p>You have not marked your attendance today.</p>
         <p>Please mark it before deadline.</p>
-        <br/>
-        <p>— Smart Hostel System</p>
       `,
     });
 
