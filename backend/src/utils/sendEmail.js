c08fa99bafd4aa2806542aef74a46874e7251cc1
Nishita-16 <nishuverma105514@gmail.com>
2026-04-13 10:45:34 +0530

    commit3

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,12 +1,12 @@
-// import nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
 
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-// });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 // export const sendEmail = async (to, name) => {
 //   try {
